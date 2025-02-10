5437fe12e1c6c4f19adf3250992e6b528bd40596
freddy konyane <148207451+freddyKto@users.noreply.github.com>
2025-02-10 10:34:20 +0200

    add code

--- src/test/java/testSample.java
+++ src/test/java/testSample.java
@@ -4,9 +4,8 @@ import java.util.ArrayList;
 public class testSample {
 
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
 
-		
+
 		dataDriven d=new dataDriven();
 	ArrayList data=d.getData("Add Profile");
 	
